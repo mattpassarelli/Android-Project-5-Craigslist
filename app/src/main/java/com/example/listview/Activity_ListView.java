5bb51b7a38f4bfffc537408a696dd8016ef0bd8e
Matt Passarelli <mattvpassarelli@gmail.com>
2017-03-29 15:41:03 -0400

    Added preferences activity and respective listener

--- app/src/main/java/com/example/listview/Activity_ListView.java
+++ app/src/main/java/com/example/listview/Activity_ListView.java
@@ -1,83 +1,131 @@
 package com.example.listview;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Activity_ListView extends AppCompatActivity {
 
 
-	ListView my_listview;
+    private SharedPreferences prefs;
+    Spinner spinner;
+    ListView my_listview;
+    private SharedPreferences.OnSharedPreferenceChangeListener listener;
+    private URL url;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		// Change title to indicate sort by
-		setTitle("Sort by:");
+        // Change title to indicate sort by
+        setTitle("Sort by:");
 
-		//listview that you will operate on
-		my_listview = (ListView)findViewById(R.id.lv);
+        //listview that you will operate on
+        my_listview = (ListView) findViewById(R.id.lv);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-		//toolbar
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
-		android.support.v7.app.ActionBar actionBar = getSupportActionBar();
 
-		setupSimpleSpinner();
+        //toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
 
-		//set the listview onclick listener
-		setupListViewOnClickListener();
+        String temp = prefs.getString("url", null);
 
-		//TODO call a thread to get the JSON list of bikes
-		//TODO when it returns it should process this data with bindData
-	}
 
-	private void setupListViewOnClickListener() {
-		//TODO you want to call my_listviews setOnItemClickListener with a new instance of android.widget.AdapterView.OnItemClickListener() {
-	}
+        try {
+            if (temp != null) {
+                url = new URL(prefs.getString("url", getString(R.string.error)));
+            } else {
+                url = new URL("http://www.tetonsoftware.com/bikes/bikes.json");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	/**
-	 * Takes the string of bikes, parses it using JSONHelper
-	 * Sets the adapter with this list using a custom row layout and an instance of the CustomAdapter
-	 * binds the adapter to the Listview using setAdapter
-	 *
-	 * @param JSONString  complete string of all bikes
-	 */
-	private void bindData(String JSONString) {
 
-	}
+        listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                try {
+                    url = new URL(prefs.getString("url", getString(R.string.error)));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(getApplicationContext(), "Site changed to: " + url.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
 
-	Spinner spinner;
-	/**
-	 * create a data adapter to fill above spinner with choices(Company,Location and Price),
-	 * bind it to the spinner
-	 * Also create a OnItemSelectedListener for this spinner so
-	 * when a user clicks the spinner the list of bikes is resorted according to selection
-	 * dontforget to bind the listener to the spinner with setOnItemSelectedListener!
-	 */
-	private void setupSimpleSpinner() {
+        prefs.registerOnSharedPreferenceChangeListener(listener);
 
-	}
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
+        setupSimpleSpinner();
 
-		default:
-			break;
-		}
-		return true;
-	}
+        //set the listview onclick listener
+        setupListViewOnClickListener();
+
+        //TODO call a thread to get the JSON list of bikes
+        //TODO when it returns it should process this data with bindData
+    }
+
+    private void setupListViewOnClickListener() {
+        //TODO you want to call my_listviews setOnItemClickListener with a new instance of android.widget.AdapterView.OnItemClickListener() {
+    }
+
+    /**
+     * Takes the string of bikes, parses it using JSONHelper
+     * Sets the adapter with this list using a custom row layout and an instance of the CustomAdapter
+     * binds the adapter to the Listview using setAdapter
+     *
+     * @param JSONString complete string of all bikes
+     */
+    private void bindData(String JSONString) {
+
+    }
+
+
+    /**
+     * create a data adapter to fill above spinner with choices(Company,Location and Price),
+     * bind it to the spinner
+     * Also create a OnItemSelectedListener for this spinner so
+     * when a user clicks the spinner the list of bikes is resorted according to selection
+     * dontforget to bind the listener to the spinner with setOnItemSelectedListener!
+     */
+    private void setupSimpleSpinner() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                Intent myIntent = new Intent(this, activityPreference.class);
+                startActivity(myIntent);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/listview/activityPreference.java
+++ app/src/main/java/com/example/listview/activityPreference.java
@@ -3,24 +3,36 @@
  */
 package com.example.listview;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 
 public class activityPreference extends PreferenceActivity {
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//TODO your stuff here
+
+		setTitle(getString(R.string.settings));
+
+		// Display the fragment as the main content.
+		FragmentManager mFragmentManager = getFragmentManager();
+		FragmentTransaction mFragmentTransaction = mFragmentManager
+				.beginTransaction();
+		PrefsFragment mPrefsFragment = new PrefsFragment();
+		mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
+		mFragmentTransaction.commit();
 	}
-	
+
 	public static class PrefsFragment extends PreferenceFragment {
-	
+
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
-			//TODO your stuff here
+
+			// Load the preferences from an XML resource
+			addPreferencesFromResource(R.xml.preferences);
 		}
 	}
 }
